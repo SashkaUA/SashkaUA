b7bd276e7ef57e65a321783a4cd4f88314f5f126
SashkaUA <106255838+SashkaUA@users.noreply.github.com>
2023-11-17 07:59:42 +0200

    Update 123.gd

--- 123.gd
+++ 123.gd
@@ -1,1 +1,12 @@
 
+var tracer:flot
+    while (true)
+    {
+        if(tracer>=0):
+            tracer + 0.1
+        
+        else{
+            while(tracer = 0)
+            tracer - 0.1
+        }
+    }
